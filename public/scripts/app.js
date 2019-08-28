78e434b24fe8941ecfa19aa71abf8e894d8228b3
Cem Alemdar <cemalemdar98@gmail.com>
2019-08-28 15:10:20 -0400

    pixi in a seperate file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('mycanvas');
 
-const app = new PIXI.Application({
+const game = new PIXI.Application({
     view: canvas,
     width: window.innerWidth,
     height: window.innerHeight
@@ -8,16 +8,16 @@ const app = new PIXI.Application({
 
 const renderer = new PIXI.Renderer({});
 
-const texture = PIXI.Texture.from('husky.png');
+const texture = PIXI.Texture.from('../img/husky.png');
 const husky = new PIXI.Sprite(texture);
 
-husky.x = app.renderer.width / 2;
-husky.y = app.renderer.height / 2;
+husky.x = game.renderer.width / 2;
+husky.y = game.renderer.height / 2;
 husky.anchor.x = 0.5;
 husky.anchor.y = 0.5;
 
-app.stage.addChild(husky);
-app.ticker.add(animate);
+game.stage.addChild(husky);
+game.ticker.add(animate);
 
 function animate() {
     husky.rotation += 0.01;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    app = express(),
+    port = (process.env.PORT || 3000),
+    mime = require('mime');
+    bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname + '/public/img')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/img/:filename', function(req, res) {
+    const filename = req.params["filename"];
+    const extensionIndex = filename.lastIndexOf('.');
+    const extension = filename.slice(extensionIndex, filename.length);
+
+    res.header('Content-Type', mime.getType(extension));
+    res.sendFile(path.join(__dirname + '/public/img/' + filename ));
+
+    console.log('/img/' + filename);
+
+});
+
+app.get('/scripts/app.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/scripts/app.js'));
+});
+
+let server = http.createServer(app);
+server.listen(port, function () {
+    console.log('server started running');
+});
