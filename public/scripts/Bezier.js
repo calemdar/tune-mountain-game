bf18f6fb14bf93b0ef49cfff4ae44c06c8d74eaf
Unknown <jsthompson@wpi.edu>
2019-09-12 15:20:04 -0400

    Parallax scrolling working

--- public/scripts/Bezier.js
+++ public/scripts/Bezier.js
@@ -99,10 +99,48 @@ const canvas = document.getElementById("mycanvas");
 const game = new PIXI.Application({
 	view: canvas,
 	width: window.innerWidth,
-	height: window.innerHeight,
+	height: 384,
 	antialias: true
 });
+/**
+ *  Parallax Scrolling Logic
+ */
+var texture1 = PIXI.Texture.from("../img/bg-far.png");
 
+const tilingSprite1 = new PIXI.TilingSprite(
+	texture1,
+	game.screen.width,
+	game.screen.height,
+);
+game.stage.addChild(tilingSprite1);
+
+tilingSprite1.position.x = 0;
+tilingSprite1.position.y = 0;
+tilingSprite1.tilePosition.x = 0;
+tilingSprite1.tilePosition.y = 0;
+
+var texture2 = PIXI.Texture.from("../img/bg-mid.png");
+
+const tilingSprite2 = new PIXI.TilingSprite(
+	texture2,
+	game.screen.width,
+	game.screen.height,
+);
+game.stage.addChild(tilingSprite2);
+
+tilingSprite2.position.x = 0;
+tilingSprite2.position.y = 128;
+tilingSprite2.tilePosition.x = 0;
+tilingSprite2.tilePosition.y = 0;
+
+game.ticker.add(() => {
+	tilingSprite1.tilePosition.x -= 0.128;
+	tilingSprite2.tilePosition.x -= 0.64;
+});
+
+/**
+ * Bezier Curve Logic
+ */
 const bezier = new PIXI.Graphics();
 const points = new PIXI.Graphics();
 
