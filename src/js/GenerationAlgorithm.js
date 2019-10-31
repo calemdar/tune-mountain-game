d233129ccd34cec132ab329bcbb2b7adfe5c5a32
Cem Alemdar <cemalemdar98@gmail.com>
2019-10-31 18:45:35 -0400

    fixed physics curve

--- src/js/GenerationAlgorithm.js
+++ src/js/GenerationAlgorithm.js
@@ -1,5 +1,7 @@
 const PIXI = require("pixi.js");
 const Planck = require("planck-js");
+let songAnalysis = require("../../static/json/SmokeandGunsAnalysis");
+let songFeatures = require("../../static/json/SmokeandGunsFeatures");
 const Vec2 = Planck.Vec2;
 
 
@@ -27,9 +29,14 @@ function GenerationAlgoritm (audioAnalysis, audioFeatures){
 
 		c0 = Vec2(getRandomInt(start.x, end.x), getRandomInt(start.y - 3, end.y + 3)); // get a random control point between start and end points
 		c1 = Vec2(getRandomInt(start.x, end.x), getRandomInt(start.y - 3, end.y + 3)); // get a random control point between start and end points
+		/*
+		cMax = Vec2(end.x + (audioFeatures.valence * 100), start.y + (audioFeatures.energy * 100)); // create control Box
+		cMin = Vec2(start.x - (audioFeatures.valence * 100), end.y + (audioFeatures.energy * 100));
 
-		//cMax = Vec2(end.x + (audioFeatures.energy * 100), );
-		//c0 = Vec2();
+		let divideBox = (cMax.x - cMin.x) / audioFeatures.time_signiture;
+		c0 = Vec2(cMin.x + (divideBox), cMin.y + (audioFeatures.danceability));
+
+		 */
 
 		singleCurvePoints.push(start, c0, c1, end);
 		curves.push(singleCurvePoints);
