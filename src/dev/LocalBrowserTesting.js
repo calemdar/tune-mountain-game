5246b75d76cf337e1b2bb192360fd991416d69ab
Unknown <jsthompson@wpi.edu>
2019-11-08 18:17:26 -0500

    Small change

--- src/dev/LocalBrowserTesting.js
+++ src/dev/LocalBrowserTesting.js
@@ -1,17 +1,17 @@
 const {BehaviorSubject} = require("rxjs");
 const exampleAnalysis = require("../../static/json/SmokeandGunsAnalysis"),
-      exampleFeatures = require("../../static/json/SmokeandGunsFeatures");
+	exampleFeatures = require("../../static/json/SmokeandGunsFeatures");
 const Game = require("../js/Game");
 
 // script that will run on website --> root for bundle
 
 // this will immediately emit a generate message with generic analysis and features
 const mockStateController = new BehaviorSubject({
-    "state": "GENERATE",
-    "body": {
-        "features": exampleFeatures,
-        "analysis": exampleAnalysis
-    }
+	"state": "GENERATE",
+	"body": {
+		"features": exampleFeatures,
+		"analysis": exampleAnalysis
+	}
 });
 
 // if you want to test state switching, do the following:
